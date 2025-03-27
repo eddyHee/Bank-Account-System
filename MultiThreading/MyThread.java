e79d068c4ebb1c3e90615b620e7207d0574bd47f
Eddy He <eddyheee@gmail.com>
2025-03-27 11:25:02 -0500

    add example for multi thread

---
+++ MultiThreading/MyThread.java
@@ -0,0 +1,24 @@
+
+class MyThread implements Runnable {
+
+    String thrdName;
+
+    public MyThread(String name) {
+        thrdName = name;
+    }
+
+    public void run() {
+        System.out.println(thrdName + " starting.");
+        try {
+            for (int counter = 0; counter < 10; counter++) {
+                Thread.sleep(4000);
+                System.out.println("In " + thrdName + ", count is " + counter);
+            }
+        } catch (InterruptedException exc) {
+            System.out.println(thrdName + " interrupted.");
+        }
+
+        System.err.println(thrdName + " terminating.");
+    }
+
+}
---
+++ MultiThreading/UseThreads.java
@@ -0,0 +1,24 @@
+
+class UseThreads {
+
+    public static void main(String[] args) {
+        System.out.println("Main thread starting.");
+
+        MyThread mt = new MyThread("Child #1");
+
+        Thread newThread = new Thread(mt);
+
+        newThread.start();
+
+        for (int i = 0; i < 50; i++) {
+            System.out.print(".");
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException exc) {
+                System.out.println("Main thread interrupted.");
+            }
+        }
+        System.out.println("Main thread ending.");
+    }
+}
